5762185ff68ffaa05cbdd362527eeb26a68c1303
fraser <mrfrase3@gmail.com>
2019-11-03 16:17:43 +0800

    support board properties abstract complicated structures

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import feathersVuex from 'feathers-vuex';
+import merge from 'lodash/merge';
 import feathersClient from './feathers-client';
 
 const { service, FeathersVuex } = feathersVuex(feathersClient, { idField: '_id' });
@@ -56,7 +57,28 @@ const store = new Vuex.Store({
         isCustom: true,
       },
     }),
-    service('boards'),
+    service('boards', {
+      instanceDefaults() {
+        return {
+          get config() {
+            if (!this.config_options) return {};
+            const config = {};
+            this.config_options.forEach((con) => {
+              config[con.option] = (con.values.find(val => val.selected) || {}).value;
+            });
+            return config;
+          },
+          get props() {
+            if (!this.properties) return {};
+            if (!this.config_options || !this.properties.menu) return this.properties;
+            return Object.keys(this.config).reduce((a, i) => merge(
+              a,
+              ((this.properties.menu[i] || {})[this.config[i]] || {}),
+            ), this.properties);
+          },
+        };
+      },
+    }),
     service('cores'),
     service('libraries'),
     service('settings', {
