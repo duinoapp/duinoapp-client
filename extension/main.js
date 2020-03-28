7e72c983edb322efd823dcd1cf9fa7b4c268fbfe
fraser <mrfrase3@gmail.com>
2020-03-28 14:02:19 +0800

    modernise extension

--- extension/main.js
+++ extension/main.js
@@ -1,134 +1,132 @@
-/*global chrome, document:false */
-'use strict';
+/* global chrome, document:false */
 
-var SerialPort = require('browser-serialport');
 
-//can't be global
-var serialPort;
+const SerialPort = require('browser-serialport');
 
-//data channel
-chrome.runtime.onConnectExternal.addListener(function(port) {
+// can't be global
+let serialPort;
+
+// data channel
+chrome.runtime.onConnectExternal.addListener((port) => {
   console.log('socket opened');
 
-  serialPort.on('disconnect', function (err) {
+  serialPort.on('disconnect', (err) => {
     console.log('serialport disconnected', err);
-    var resp = {};
+    const resp = {};
     resp.op = 'onDisconnect';
     port.postMessage(resp);
   });
 
-  //collapse into .error field?
-  serialPort.on('error', function (err) {
+  // collapse into .error field?
+  serialPort.on('error', (err) => {
     console.log('serialport errored', err);
-    var resp = {};
+    const resp = {};
     resp.op = 'onError';
     port.postMessage(resp);
   });
 
-  serialPort.on('close', function () {
+  serialPort.on('close', () => {
     console.log('serialport closed');
     // let other end emit close when it notices port disconnect
-    var resp = {};
+    const resp = {};
     resp.op = 'onClose';
     port.postMessage(resp);
   });
 
-  serialPort.on('data', function (data) {
+  serialPort.on('data', (data) => {
     console.log('serialport data');
-    var resp = {};
+    const resp = {};
     resp.op = 'data';
     resp.data = data;
     port.postMessage(resp);
   });
 
-  port.onMessage.addListener(function (msg) {
+  port.onMessage.addListener((msg) => {
     console.log('socket received data');
 
-    // console.log('socket received', msg);
-    //check for string as well? or force buffer sends from other side...
-    if(msg && msg.hasOwnProperty('data')){
-      var buffer = new Buffer(msg.data);
+    console.log('socket received', msg);
+    // check for string as well? or force buffer sends from other side...
+    if (msg && msg.data) {
+      const buffer = Buffer.from(msg.data);
       // console.log('writing to serial', buffer.toString('utf-8'));
-      serialPort.write(buffer, function(err, results){
+      serialPort.write(buffer, (err, results) => {
         console.log(err, results);
       });
     }
   });
 
-  port.onDisconnect.addListener(function () {
+  port.onDisconnect.addListener(() => {
     console.log('socket disconnected');
     serialPort.close();
   });
-
 });
 
-//command channel
-chrome.runtime.onMessageExternal.addListener(function(msg, sender, responder) {
-
+// command channel
+chrome.runtime.onMessageExternal.addListener((msg, sender, responder) => {
   console.log(msg);
 
-  var cmds = {
-    getManifest:function(){
-      var resp = {};
+  const cmds = {
+    getManifest() {
+      const resp = {};
       resp.data = chrome.runtime.getManifest();
       responder(resp);
     },
-    list:function(){
-      SerialPort.list(function (err, data) {
+    list() {
+      SerialPort.list((err, data) => {
         console.log(msg.op, 'err:', err, data);
-        var resp = {};
-        if (err){ resp.error = err.message; }
-        if (data){ resp.data = data; }
+        const resp = {};
+        if (err) { resp.error = err.message; }
+        if (data) { resp.data = data; }
         responder(resp);
       });
     },
-    construct:function(){
+    construct() {
       console.log('construct');
-      var resp = {};
-      serialPort = new SerialPort.SerialPort(msg.path, msg.options, false, function(err){
+      const resp = {};
+      serialPort = new SerialPort.SerialPort(msg.path, msg.options, false, ((err) => {
         console.log(msg.op, 'err:', err);
-        if (err){ resp.error = err.message; }
-      });
+        if (err) { resp.error = err.message; }
+      }));
       responder(resp);
     },
-    open:function(){
-      serialPort.open(function(err){
+    open() {
+      serialPort.open((err) => {
         console.log(msg.op, 'err:', err);
-        var resp = {};
-        if (err){ resp.error = err.message; }
+        const resp = {};
+        if (err) { resp.error = err.message; }
         responder(resp);
       });
     },
-    close:function(){
-      serialPort.close(function (err){
+    close() {
+      serialPort.close((err) => {
         console.log(msg.op, 'err:', err);
-        var resp = {};
-        if (err){ resp.error = err.message; }
+        const resp = {};
+        if (err) { resp.error = err.message; }
         responder(resp);
       });
     },
-    drain:function(){
-      serialPort.drain(function (err, data){
+    drain() {
+      serialPort.drain((err, data) => {
         console.log(msg.op, 'err:', err, data);
-        var resp = {};
-        if (err){ resp.error = err.message; }
-        if (data){ resp.data = data; }
+        const resp = {};
+        if (err) { resp.error = err.message; }
+        if (data) { resp.data = data; }
         responder(resp);
       });
     },
-    flush:function(){
-      serialPort.flush(function (err, data){
+    flush() {
+      serialPort.flush((err, data) => {
         console.log(msg.op, 'err:', err, data);
-        var resp = {};
-        if (err){ resp.error = err.message; }
-        if (data){ resp.data = data; }
+        const resp = {};
+        if (err) { resp.error = err.message; }
+        if (data) { resp.data = data; }
         responder(resp);
       });
-    }
+    },
   };
 
-  if (!cmds.hasOwnProperty(msg.op)) {
-    return responder({error:'Unknown op'});
+  if (!cmds[msg.op]) {
+    return responder({ error: 'Unknown op' });
   }
 
   cmds[msg.op]();
@@ -136,9 +134,9 @@ chrome.runtime.onMessageExternal.addListener(function(msg, sender, responder) {
   return true; // required if we want to respond after the listener
 });
 
-chrome.app.runtime.onLaunched.addListener(function() {
-  var a = document.createElement('a');
+chrome.app.runtime.onLaunched.addListener(() => {
+  const a = document.createElement('a');
   a.href = 'http://127.0.0.1:8080/';
-  a.target='_blank';
+  a.target = '_blank';
   a.click();
 });
