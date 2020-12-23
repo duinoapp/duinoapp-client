92c7c91468ac2ffb4ea849b2dea5b2d89dfd1fd5
mrfrase3 <mrfrase3@gmail.com>
2020-12-23 10:57:12 +0000

    Update gh-pages to output generated at 0860eb8

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,39 @@
-/**
- * Welcome to your Workbox-powered service worker!
- *
- * You'll need to register this file in your web app and you should
- * disable HTTP caching for this file too.
- * See https://goo.gl/nhQhGp
- *
- * The rest of the code is auto-generated. Please don't update this file
- * directly; instead, make changes to your Workbox build configuration
- * and re-run your build process.
- * See https://goo.gl/2aRDsh
- */
+importScripts("/precache-manifest.d7dc8b8c223302459a325a4265fbbcec.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+// service-worker.js
 
-importScripts(
-  "/precache-manifest.34c0ab4fda8b1ad95b6d509b5f69eccd.js"
-);
+workbox.core.setCacheNameDetails({ prefix: 'd4' });
 
-workbox.core.setCacheNameDetails({prefix: "duinoapp-client"});
+// Do not touch this line
+const LATEST_VERSION = '3.1.0';
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
+self.addEventListener('activate', (event) => {
+  console.log(`%c ${LATEST_VERSION} `, 'background: #ddd; color: #0000ff');
+  if (caches) {
+    caches.keys().then((arr) => {
+      arr.forEach((key) => {
+        if (key.indexOf('d4-precache') < -1) {
+          caches.delete(key).then(() => console.log(`%c Cleared ${key}`, 'background: #333; color: #ff0000'));
+        } else {
+          caches.open(key).then((cache) => {
+            cache.match('version').then((res) => {
+              if (!res) {
+                cache.put('version', new Response(LATEST_VERSION, { status: 200, statusText: LATEST_VERSION }));
+              } else if (res.statusText !== LATEST_VERSION) {
+                caches.delete(key).then(() => console.log(`%c Cleared Cache ${LATEST_VERSION}`, 'background: #333; color: #ff0000'));
+              } else console.log(`%c Great you have the latest version ${LATEST_VERSION}`, 'background: #333; color: #00ff00');
+            });
+          });
+        }
+      });
+    });
   }
 });
 
-/**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
- */
+workbox.skipWaiting();
+workbox.clientsClaim();
+
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
