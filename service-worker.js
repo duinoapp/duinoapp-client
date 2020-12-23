d1ee8ce4b53a2a220c3ee8e3cabe82906a3ca40c
mrfrase3 <mrfrase3@gmail.com>
2020-12-23 19:22:36 +0800

    fix undefined function in service worker

--- service-worker.js
+++ service-worker.js
@@ -28,7 +28,7 @@ self.addEventListener('activate', (event) => {
   }
 });
 
-workbox.skipWaiting();
+workbox.core.skipWaiting();
 workbox.clientsClaim();
 
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
