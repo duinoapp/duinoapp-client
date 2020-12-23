733fa88ff3bdd30410b349bcf532e237032c5e52
mrfrase3 <mrfrase3@gmail.com>
2020-12-23 11:49:09 +0000

    Update gh-pages to output generated at 83b2937

--- service-worker.js
+++ service-worker.js
@@ -34,6 +34,5 @@ workbox.core.skipWaiting();
 workbox.core.clientsClaim();
 
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.core.precaching.suppressWarnings();
-workbox.core.precaching.precacheAndRoute(self.__precacheManifest, {});
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
