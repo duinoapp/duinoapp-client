ec64ca633937d7bb63cdb0fa9c5312a449154fa7
mrfrase3 <mrfrase3@gmail.com>
2020-12-23 15:20:54 +0800

    use any version

--- bust-cache.js
+++ bust-cache.js
@@ -2,5 +2,5 @@ const fs = require('fs');
 const { version } = require('./package.json');
 
 let swFile = fs.readFileSync('./service-worker.js', 'utf-8');
-swFile = swFile.replace(/LATEST_VERSION = '[0-9.]+';/, `LATEST_VERSION = '${version}';`);
+swFile = swFile.replace(/LATEST_VERSION = '[^']+';/, `LATEST_VERSION = '${version}';`);
 fs.writeFileSync('./service-worker.js', swFile);
