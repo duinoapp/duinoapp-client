3f47c470813dbbc3640d81fe6d0667b8501f90e4
mrfrase3 <mrfrase3@gmail.com>
2020-12-23 15:11:37 +0800

    specify encoding

--- bust-cache.js
+++ bust-cache.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const { version } = require('./package.json');
 
-let swFile = fs.readFileSync('./service-worker.js');
+let swFile = fs.readFileSync('./service-worker.js', 'utf-8');
 swFile = swFile.replace(/LATEST_VERSION = '[0-9.]+';/, `LATEST_VERSION = '${version}';`);
 fs.writeFileSync('./service-worker.js', swFile);
